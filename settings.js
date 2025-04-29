bf6d2b49f9bafc1642d2f6bb4c94623b570cced0
aslesi <alesya.lys@mail.ru>
2025-04-29 09:20:22 +0000

    using API

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "edc4f1d7c8b5ab6ea4f9ea5ff7af275a",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "beppu,jp",
 };
